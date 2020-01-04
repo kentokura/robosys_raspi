b15a7f208e7dcdfec68a298e652fbf442e4e7e33
Ubuntu <ubuntu@ubuntu.mshome.net>
2020-01-04 21:37:40 +0900

    :sparkle:sushi

--- myled.c
+++ myled.c
@@ -2,6 +2,8 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/uaccess.h>
+#include <linux/io.h>
 MODULE_AUTHOR("Ken Tokura");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
@@ -10,22 +12,58 @@ MODULE_VERSION("0.1");
 static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
+static volatile u32 *gpio_base = NULL; //アドレスをマッピングするための配列
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
-	printk(KERN_INFO "led_write is called\n");
-	return 1; //読み込んだ文字数を返す(この場合はダミーの1)
+	char c; //読み込んだ字を入れる変数
+	if(copy_from_user(&c,buf,sizeof(char)))
+		return -EFAULT;
+
+	if(c == '0')
+		gpio_base[10] = 1 << 25;
+	else if(c == '1')
+		gpio_base[7] = 1 << 25;
+
+	//printk(KERN_INFO "receive %c\n",c);
+	return 1; 
 }
+
+/*----------------------------------------------------------------------------*/
+static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos)
+{
+	int size = 0;
+	char sushi[] = {0xF0,0x9F,0x8D,0xA3,0x0A}; //寿司のバイナリ 
+	if(copy_to_user(buf+size,(const char *)sushi, sizeof(sushi))){
+		printk( KERN_INFO "sushi : copy_to_user filed\n" );
+	       return -EFAULT;
+	}
+	size += sizeof(sushi);
+	return size;
+}
+
 
 static struct file_operations led_fops = {
 	.owner = THIS_MODULE,
-	.write = led_write
+	.write = led_write,
+	.read  = sushi_read
 };
 
+/*----------------------------------------------------------------------------*/
 static int __init init_mod(void) //カーネルモジュールの初期化
 {
 	// デバイス番号を取得
 	int retval;
+	
+	gpio_base = ioremap_nocache(0x3f200000, 0xA0);
+
+	const u32 led = 25;
+	const u32 index = led/10; //GPFSEL2
+	const u32 shift = (led%10)*3; //15bit
+	const u32 mask = ~(0x7 << shift); //11111111111111000111111111111111
+	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift); //001: output flag //11111111111111001111111111111111
+	
+
 	retval =  alloc_chrdev_region(&dev, 0, 1, "myled");
 	if(retval < 0){
 		printk(KERN_ERR "alloc_chrdev_region failed.\n");
@@ -60,6 +98,7 @@ static void __exit cleanup_mod(void) //後始末
 	unregister_chrdev_region(dev, 1); //デバイス番号の開放
 	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
+
 
 module_init(init_mod); //マクロで関数を登録
 module_exit(cleanup_mod); //同上
